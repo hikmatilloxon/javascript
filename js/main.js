a110f1fdd762fa9db8b95c4c36d54a4541228ef8
hikmatilloxon <hikmatilloabdulaziziy@gmail.com>
2022-08-04 09:07:23 +0500

    javascript

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var name = prompt("Ismingiz")
+alert(name)
+
+var familya = prompt("Familyangiz")
+alert(familya)
+
+var sharifingiz = prompt("Sharifingiz")
+alert(sharifingiz)
+
+var yoshingiz = prompt("Yoshingiz")
+alert(yoshingiz)
+
+var tugilganYil = prompt("Tugilgan yilingiz")
+alert(tugilganYil)
+
+var telRaqamingiz = prompt("Tel raqamingiz")
+alert(telRaqamingiz)
+
+var shahar = prompt("Shahringiz")
+alert(shahar)
+
+var qayerdaIshlaysiz = prompt("Qayerda ishlaysiz")
+alert(qayerdaIshlaysiz)
+
+var otangizningKasbi = prompt("Otangizning kasbi")
+alert(otangizningKasbi)
+
+var onangizningKasbi = prompt("Onangizning kasbi")
+alert(onangizningKasbi)
+
